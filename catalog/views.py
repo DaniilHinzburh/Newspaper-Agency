c0e35be3ece7eac5a6b789801fe0b0760072532d
ilgan <2ilgan1601@gmail.com>
2024-02-04 00:54:42 +0300

    Add views for managing topics in the catalog section.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Redactor, Topic, Newspaper
 
-# Create your views here.
+
+def index(request):
+    return render(request, "catalog/index.html")
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "catalog/topic_list.html"
+    paginate_by = 5
+
+
+class TopicCreateView(generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:topic-list")
+
+
+class TopicUpdateView(generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:topic-list")
+
+
+class TopicDeleteView(generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("catalog:topic-list")
