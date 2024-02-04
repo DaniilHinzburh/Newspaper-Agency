d24f51306aa8d4ffd1941cba622e53e5dd44bba8
ilgan <2ilgan1601@gmail.com>
2024-02-04 14:49:17 +0300

    feat: add NewspaperListView,NewspaperCreateView,NewspaperUpdateView,NewspaperDeleteView

--- catalog/views.py
+++ catalog/views.py
@@ -60,3 +60,30 @@ class RedactorDeleteView(generic.DeleteView):
     model = Redactor
     success_url = reverse_lazy("catalog:redactor-list")
     template_name = "catalog/redactor_delete_form.html"
+
+
+
+class NewspaperListView(generic.ListView):
+    model = Newspaper
+    context_object_name = "newspaper_list"
+    template_name = "catalog/newspaper_list.html"
+    paginate_by = 5
+
+
+class NewspaperCreateView(generic.CreateView):
+    model = Newspaper
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:newspaper-list")
+    template_name = "catalog/newspaper_form.html"
+
+
+class NewspaperUpdateView(generic.UpdateView):
+    model = Newspaper
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:newspaper-list")
+
+
+class NewspaperDeleteView(generic.DeleteView):
+    model = Newspaper
+    success_url = reverse_lazy("catalog:newspaper-list")
+    template_name = "catalog/newspaper_delete_form.html"
