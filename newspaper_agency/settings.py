af2b74147044cad90afc1fd79e415cd67eab532d
ilgan <2ilgan1601@gmail.com>
2024-02-10 15:01:43 +0300

    chore: moved the SECRET_KEY to a .env file

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -9,7 +9,8 @@ https://docs.newspaper_agency.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.newspaper_agency.com/en/5.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.newspaper_agency.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&d8!$bm*h$jrr9*rea2b+s8@88fqz26_u5!j9^5p0*(w(o2s%e'
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
